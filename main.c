b8bfd6476444bbd301c55e23277b7aadd37a89e9
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-07-31 02:51:37 +0800

    Add current implement, still have some bug
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- main.c
+++ main.c
@@ -38,7 +38,7 @@ struct CPU cpu_pc_add(struct CPU cpu, char val)
 
   if (cpu.pc_l > 9) {
     cpu.pc_h += 1;
-    cpu.pc_l = val - 10;
+    cpu.pc_l -= 10;
   }
 
   return cpu;
@@ -68,7 +68,7 @@ void execute(char *memory) {
 
   while (!_exit) {
     pc = cpu_pc_get(cpu);
-    //  printf("mem[%d] = %d, r1 = %d, r2 = %d\n", pc, memory[pc], cpu.r1, cpu.r2);
+    printf("mem[%d] = %d, r1 = %d, r2 = %d, z = %d\n", pc, memory[pc], cpu.r1, cpu.r2, cpu.z);
 
     switch (memory[pc]) {
     case 0:
@@ -86,6 +86,7 @@ void execute(char *memory) {
       assert((tmp1 < 2) && "Whoops, we only support r1,r2 register in INC command.");
       if (tmp1 == 0) cpu.r1++;
       if (tmp1 == 1) cpu.r2++;
+//      printf("inc %s\n", (tmp1 == 0) ? "r1" : "r2");
       cpu = cpu_pc_add(cpu, 2);
       break;
 
@@ -95,7 +96,9 @@ void execute(char *memory) {
       cpu.r3 = tmp1 / 10;
       cpu.r4 = tmp1 % 10;
 //      printf("R3 = %d, R4 = %d, tmp1 = %d, m1 = %d, m2 = %d\n", cpu.r3, cpu.r4, tmp1, memory[pc + 1] , memory[pc + 2]);
+//      printf("PC = %d, PC_H = %d, PC_L = %d\n", cpu_pc_get(cpu), cpu.pc_h, cpu.pc_l);
       cpu = cpu_pc_add(cpu, 3);
+//      printf("PC = %d, PC_H = %d, PC_L = %d\n", cpu_pc_get(cpu), cpu.pc_h, cpu.pc_l);
       break;
 
     case 4:
@@ -104,6 +107,7 @@ void execute(char *memory) {
       assert((tmp1 < 2) && "Whoops, we only support r1,r2 register in MOV command.");
       if (tmp1 == 0) cpu.r1 = tmp2;
       if (tmp1 == 1) cpu.r2 = tmp2;
+      printf("mov %s %d\n", ((tmp1 == 0) ? "r1" : "r2") , tmp2);
       cpu = cpu_pc_add(cpu, 3);
       break;
 
@@ -111,18 +115,36 @@ void execute(char *memory) {
       tmp1 = memory[pc + 1]; // rx
       tmp2 = memory[pc + 2]; // value
       cpu.z = (tmp1 == tmp2) ? 1 : 0;
+
+      if (tmp1 == 0) {
+        tmp1 = cpu.r1;
+      }
+      if (tmp1 == 1) {
+        tmp1 = cpu.r2;
+      }
+
+      if (tmp1 >= tmp2) {
+        cpu.z = 1;
+      }
+      else if (tmp1 <= tmp2) {
+        cpu.z = 2;
+      }
+      else {
+        cpu.z = 0;
+      }
+
       cpu = cpu_pc_add(cpu, 3);
-      printf("cmp %s %d\n", (tmp1 == 0)? "r1" : "r2", tmp2);
+      printf("cmp %s %d, rx = %d\n", (tmp1 == 0)? "r1" : "r2", tmp2, cpu.r2);
       break;
 
     case 6:
       tmp1 = memory[pc + 1]; // PC_H
       tmp2 = memory[pc + 2]; // PC_L
-      if (0 == cpu.z) {
+      if (1 == cpu.z) {
         cpu.pc_h = tmp1;
         cpu.pc_l = tmp2;
         cpu.z = 0;
-//        printf("bne %d %d\n", tmp1, tmp2);
+        printf("bne %d %d\n", tmp1, tmp2);
       }
       else {
         cpu = cpu_pc_add(cpu, 3);
@@ -140,17 +162,17 @@ int main(int argc, char *argv[]) {
     4, 0, 0,  // mov r1 0      ; i = 0
               //
               // _for_i: 3
-    4, 1, 1,  //   mov r1 1    ; j = 1
+    4, 1, 1,  //   mov r2 1    ; j = 1
     2, 0,     //   inc r1      ; i++
               //
               // _for_j: 8
     3, 0, 1,  //   mul r1 r2   ; i * j
     1,        //   print       ; print("R1 x R2 = Result")
     2, 1,     //   inc r2      ; j++
-    5, 1, 10, //   cmp r2 10   ; if (j <= 10)
+    5, 1, 10,  //   cmp r2 9   ; if (j <= 9)
     6, 0, 8,  //   bne _for_j  ;   goto _for_j
               //
-    5, 0, 10, //   cmp r1 10   ; if (i <= 10)
+    5, 0, 10,  //   cmp r1 9   ; if (i <= 9)
     6, 0, 3,  //   bnc _for_i  ;   goto _for_i
               //
     0,        //   exit        ; Exit application
