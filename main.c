fbbf95d61293268226c9dab4562b58c9f9b8b2a0
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-07-31 15:37:28 +0800

    make dprintf output to stdout
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- main.c
+++ main.c
@@ -5,8 +5,9 @@
 
 #define DEBUG 0
 #define dprintf(fmt, ...) do {                                  \
-                if (DEBUG)                                      \
-                        fprintf(stderr, fmt, ##__VA_ARGS__);    \
+                if (DEBUG) {                                    \
+                        fprintf(stdout, fmt, ##__VA_ARGS__);    \
+                }                                               \
         } while (0)
 
 struct CPU {
